be421da99f7ca76aaf45c3698b498d8b6a4103fa
MadMakZa <immakza@gmail.com>
2021-06-14 13:50:33 +0300

    Job scheduler work fine

--- app/src/main/java/maxim/sky/testpushparsjson/JobSchedulerService.kt
+++ app/src/main/java/maxim/sky/testpushparsjson/JobSchedulerService.kt
@@ -2,14 +2,44 @@ package maxim.sky.testpushparsjson
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.util.Log
 
 class JobSchedulerService: JobService() {
 
+    private var jobCancelled: Boolean = false
+
     override fun onStartJob(params: JobParameters?): Boolean {
+        Log.d("test","Job started")
+        doBackgroundWork(params)
+        return true
+    }
+
+
+    private fun doBackgroundWork(params: JobParameters?){
+        Thread(Runnable {
+            for (i in 1..10) {
+                if (jobCancelled) {
+                    return@Runnable
+                }
+                Log.d("test", "Thread wisp $i")
+                try {
+                    Thread.sleep(500)
+                } catch (e: InterruptedException) {
+
+                }
+            }
+            Log.d("test","Job finished")
+            jobFinished(params, false)
+        }).start()
+
+        }
+
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        Log.d("test","Job stopped")
+        jobCancelled = true
         return false
     }
 
-    override fun onStopJob(params: JobParameters?): Boolean {
-        return true
-    }
+
 }
--- app/src/main/java/maxim/sky/testpushparsjson/MainActivity.kt
+++ app/src/main/java/maxim/sky/testpushparsjson/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.ComponentName
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
     private lateinit var textView: TextView
     private val JOB_ID = 1
+    private var flagBtn: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +25,18 @@ class MainActivity : AppCompatActivity() {
 
         button = findViewById(R.id.btn_on_off)
         textView = findViewById(R.id.tv_text)
-        createObjectJobInfo()
 
         button.setOnClickListener {
-
+            if (!flagBtn){
+                flagBtn = true
+                button.text = "OFF"
+                createObjectJobInfo()
+            }
+            else if (flagBtn){
+                flagBtn = false
+                button.text = "ON"
+                cancelJob()
+            }
         }
 
     }
@@ -40,20 +50,29 @@ class MainActivity : AppCompatActivity() {
         val serviceName = ComponentName(this, JobSchedulerService::class.java)
         val jobInfo = JobInfo.Builder(JOB_ID, serviceName)
             .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
-            .setRequiresDeviceIdle(true)
-            .setRequiresCharging(true)
-            .setPeriodic(1*60*1000)
+//            .setRequiresDeviceIdle(true)
+            .setRequiresCharging(false)
+            .setPersisted(true)
+            .setPeriodic(15*60*1000) //15 минут минимум
             .build()
 
         //task
         val scheduler: JobScheduler = applicationContext.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
         val result = scheduler.schedule(jobInfo)
-        if (result==JobScheduler.RESULT_SUCCESS) {
-            Log.d("test","JOb Scheduled");
+        if (result == JobScheduler.RESULT_SUCCESS) {
+            Log.d("test","JOb Scheduled created");
         } else {
             Log.d("test","Job Scheduling fail");
         }
     }
+
+    private fun cancelJob(){
+        val scheduler: JobScheduler = applicationContext.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        scheduler.cancel(JOB_ID)
+        Log.d("test","Job Cancelled");
+    }
+
+
 
 
 
